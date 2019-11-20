653d591c1d79a843f7da037b34924b8c20ab89b6
GetRhymes <tarasenko.ns@edu.spbstu.ru>
2019-11-20 14:44:00 +0300

    new tests

--- src/test/kotlin/Test.kt
+++ src/test/kotlin/Test.kt
@@ -5,37 +5,72 @@ import kotlin.test.assertEquals
 
 class Test {
 
+    private val boardTest1 = arrayOf(
+        charArrayOf('_', '_', '_', '_', '_', '_', '_', '_'),
+        charArrayOf('_', '_', '_', '_', 'W', '_', '_', '_'),
+        charArrayOf('_', '_', '_', 'W', 'W', '_', '_', '_'),
+        charArrayOf('_', '_', '_', 'W', 'W', '_', '_', '_'),
+        charArrayOf('_', '_', '_', 'W', 'B', '_', '_', '_'),
+        charArrayOf('_', '_', '_', '_', '_', '_', '_', '_'),
+        charArrayOf('_', '_', '_', '_', '_', '_', '_', '_'),
+        charArrayOf('_', '_', '_', '_', '_', '_', '_', '_')
+    )
+
+    private val boardTest2 = arrayOf(
+        charArrayOf('_', '_', '_', '_', '_', '_', '_', '_'),
+        charArrayOf('_', '_', '_', '_', '_', '_', '_', '_'),
+        charArrayOf('_', '_', '_', 'W', '_', '_', '_', '_'),
+        charArrayOf('_', '_', '_', 'W', 'W', '_', '_', '_'),
+        charArrayOf('_', '_', '_', 'W', 'B', 'W', 'W', '_'),
+        charArrayOf('_', '_', '_', '_', '_', '_', '_', '_'),
+        charArrayOf('_', '_', '_', '_', '_', '_', '_', '_'),
+        charArrayOf('_', '_', '_', '_', '_', '_', '_', '_')
+    )
+
+    private val boardTest3 = arrayOf(
+        charArrayOf('_', '_', '_', '_', '_', '_', '_', '_'),
+        charArrayOf('_', '_', '_', '_', '_', '_', 'W', '_'),
+        charArrayOf('_', '_', '_', 'W', '_', '_', 'W', '_'),
+        charArrayOf('_', '_', '_', 'W', 'W', '_', 'W', '_'),
+        charArrayOf('_', 'W', 'W', 'W', 'B', 'W', 'W', '_'),
+        charArrayOf('_', '_', '_', '_', '_', '_', 'B', '_'),
+        charArrayOf('_', '_', '_', '_', '_', '_', '_', '_'),
+        charArrayOf('_', '_', '_', '_', '_', '_', '_', '_')
+    )
+
+    private val mop = Opponent()
+    private fun move2(board: Array<CharArray>): Int {
+
+        val listMax = mutableMapOf<Pair<Int, Int>, Int>()
+        val moveOption = mop.move(board, 'W', 'B')
+        for (i in moveOption) {
+            var count = 0
+            for (el in i.value) {
+                count += el.first
+            }
+            listMax[i.key] = count
+        }
+        println(mop.move(board, 'B', 'W'))
+        println(listMax)
+        println(listMax.maxBy { it.value })
+        val result = listMax.maxBy { it.value }
+        return result!!.value
+    }
     @Test
     @Tag("Easy")
-    fun testMove() {
-        val mop = Opponent()
-        fun move2(): Int {
+    fun testMove1() {
+        assertEquals(4, move2(boardTest1))
+    }
 
-            val board = arrayOf(
-                charArrayOf('_', '_', '_', '_', '_', '_', '_', '_'),
-                charArrayOf('_', '_', '_', '_', '_', '_', '_', '_'),
-                charArrayOf('_', '_', '_', 'W', '_', '_', '_', '_'),
-                charArrayOf('_', '_', '_', 'W', 'W', '_', '_', '_'),
-                charArrayOf('_', '_', '_', 'W', 'B', '_', '_', '_'),
-                charArrayOf('_', '_', '_', '_', '_', '_', '_', '_'),
-                charArrayOf('_', '_', '_', '_', '_', '_', '_', '_'),
-                charArrayOf('_', '_', '_', '_', '_', '_', '_', '_')
-            )
-            val listMax = mutableMapOf<Pair<Int, Int>, Int>()
-            val moveOption = mop.move(board, 'W', 'B')
-            for (i in moveOption) {
-                var count = 0
-                for (el in i.value) {
-                    count += el.first
-                }
-                listMax[i.key] = count
-            }
-            println(mop.move(board, 'B', 'W'))
-            println(listMax)
-            println(listMax.maxBy { it.value })
-            val result = listMax.maxBy { it.value }
-            return result!!.value
-        }
-        assertEquals(2, move2())
+    @Test
+    @Tag("Easy")
+    fun testMove2() {
+        assertEquals(3, move2(boardTest2))
+    }
+
+    @Test
+    @Tag("Easy")
+    fun testMove3() {
+        assertEquals(5, move2(boardTest3))
     }
 }
